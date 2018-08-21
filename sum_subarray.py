ad525a21ebd35076add0d91a73d822301a2ac1e2
Zoe Yang <yiyingy@splunk.com>
2018-08-21 18:02:38 +0800

    first

--- sum_subarray.py
+++ sum_subarray.py
@@ -2,5 +2,19 @@
 class Solution:
     def FindGreatestSumOfSubArray(self, array):
         # write code here
+        if not array:
+            return 0
+        res = array[0]
+        for index,data in enumerate(array):
+            res = max(res, self.end_with(index, array))
+        return res
+
+    def end_with(self,index, array):
+        if index == 0:
+            return array[0]
+        return max(self.end_with(index-1,array)+array[index], array[index])
+
+
+
 
 print(Solution().FindGreatestSumOfSubArray([6,-3,-2,7,-15,1,2,2]))
